9f0c99f91caa3849854a71451dbdab6ee3b63edc
João Gabriel Lima <joaogabriellima@MacBook-Air-de-Joao.local>
2023-04-23 22:38:59 -0300

    Initial

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
+    'local': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     ),
-    'local': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
